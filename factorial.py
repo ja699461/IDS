7fc2fdb8e7081b7c3f99b1253bf68f11d4d0017a
Babak Forouraghi <bforoura@sju.edu>
2022-02-07 15:05:56 -0500

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-# The recursive facorial function
-def factorial(n):
-     if (n < 0):
-          return -1
-     elif (n==0):
-          return 1
-     else:
-          return n * factorial(n-1)
-     
